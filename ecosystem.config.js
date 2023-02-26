21a92b88193b5ea12a3e7f6a98390e3fd106c9c1
taejun <joon5006@naver.com>
2023-02-26 16:04:00 +0900

    chore: deploy 수정. 폴더 및 port 여러개 생성

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: "app",
+      name: "collector1",
       script: "build/index.js",
       watch: false,
       autorestart: false,
       instances: 1,
       exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 5001,
+      },
+    },
+    {
+      name: "collector2",
+      script: "build/index.js",
+      watch: false,
+      autorestart: false,
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 5002,
+      },
     },
   ],
 };
