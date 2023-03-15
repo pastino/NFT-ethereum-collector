773dd1b78fc9f1185ba08222bb9045121aff1723
taejun <joon5006@naver.com>
2023-03-15 10:13:56 +0900

    fix: opensea api 기반에서 alchemy api로 변경하기로 결정 - 코드 엎기 전 마지막 commit

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@ import kakaoAuthorization from "./routes/kakaoAuthorization";
 import deleteCollectionData from "./routes/deleteCollectionData";
 import createWalletAndCollection from "./routes/createWalletAndCollection";
 import { Collection } from "./entities/Collection";
+
+import { Alchemy, Network } from "alchemy-sdk";
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 import axios from "axios";
-import { PROXY_LIST_2 } from "./commons/proxyList";
-import { getOpenseaApiKey, sleep } from "./commons/utils";
 
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
@@ -46,51 +47,95 @@ const deleteNotCompleteCollection = async () => {
     console.log(e);
   }
 };
+const collectionAbi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        name: "",
+        type: "string",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        name: "",
+        type: "string",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
 
 const proxyTest = async () => {
-  const successProxyList: any = [];
+  const config = {
+    apiKey: process.env.ALCHEMY_API_KEY,
+    network: Network.ETH_MAINNET,
+  };
+  const alchemy = new Alchemy(config);
 
-  for (let i = 0; i < PROXY_LIST_2.length; i++) {
-    const headerConfig: any = {
-      // httpsAgent: new HttpsProxyAgent("http://43.133.45.244:19886" as string),
-      // httpAgent: new HttpsProxyAgent("http://43.133.45.244:19886" as string),
-      // proxy: false,
-      headers: {
-        "X-API-KEY": getOpenseaApiKey(),
-      },
-    };
+  //0x6c5407d1c272436a66ecbc6a8110f790d5821606
+  // const data = await alchemy.nft.getContractsForOwner(
+  //   "0x7ef61cacd0c785eacdfe17649d1c5bcba676a858"
+  // );
+  const data = await alchemy.nft.getContractMetadata(
+    "0xabe9d0e4ad08e605d57a0c10f9d13e0e7283ea50"
+  );
+  console.log("data", data);
 
-    try {
-      const result = await axios.get(
-        `https://api.opensea.io/api/v1/collections?asset_owner=0x7ef61cacd0c785eacdfe17649d1c5bcba676a858&offset=0&limit=300`,
-        headerConfig
-      );
+  // const apiKey = "izPO5-h0NhIZdoCTq3sHPQWCNnGKEUbq";
+  // const walletAddress = "0x7ef61cacd0c785eacdfe17649d1c5bcba676a858";
 
-      delete result.data;
-      // console.log(result.config.httpsAgent);
-      // console.log(result.config.httpAgent);
+  // const options = {
+  //   url: `https://api.algorithmia.com/v1/web3/${walletAddress}/collections`,
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization: `Bearer ${apiKey}`,
+  //   },
+  // };
 
-      if (typeof result?.data === "string") {
-        throw new Error("에러");
-      }
-      successProxyList.push(PROXY_LIST_2?.[i]?.host, PROXY_LIST_2?.[i]?.pory);
-      console.log("성공~!", PROXY_LIST_2?.[i]?.host, PROXY_LIST_2?.[i]?.pory);
-      await sleep(1.5);
-    } catch (e: any) {
-      console.log("e", e.message);
-      await sleep(1.5);
-      console.log("에라이~!");
-    }
-  }
-  console.log("successProxyList", successProxyList);
+  // const response = await axios.get(
+  //   `https://api.algorithmia.com/v1/web3/${walletAddress}/collections`,
+  //   {
+  //     "Content-Type": "application/json",
+  //     Authorization: `Bearer ${apiKey}`,
+  //   } as any
+  // );
+
+  // console.log("response", response);
 };
 
 createConnection(connectionOptions)
   .then(() => {
     console.log("DB CONNECTION!");
     app.listen(PORT, async () => {
+      proxyTest();
       console.log(`Listening on port: "http://localhost:${PORT}"`);
-      console.log(getOpenseaApiKey());
       if (IS_PRODUCTION) {
         await deleteNotCompleteCollection();
       }
