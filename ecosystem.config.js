7d6e0ec01e61689a7fb725c67cbd0e1e63382c92
taejun <joon5006@naver.com>
2023-03-26 07:22:40 +0900

    chore: pm2 설정 변경 - ecosystem.config.js 파일 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "app",
+      name: "collector",
       script: "build/index.js",
       watch: false,
       autorestart: false,
       instances: 1,
       exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 5000,
+      },
     },
   ],
 };
